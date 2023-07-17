5f5cb6626f30e52b57ffa5c48b23345a6a256a10
TheAyush101 <139744349+TheAyush101@users.noreply.github.com>
2023-07-17 14:26:43 +0530

    Add files via upload

---
+++ Express/blogexpress/data/blogs.js
@@ -0,0 +1,24 @@
+blogs = [
+  {
+    title: "how to get started with python",
+    content: "This is content of python",
+    slug: "python-learn",
+  },
+  {
+    title: "how to get started with js",
+    content: "This is content of js",
+    slug: "js-learn",
+  },
+  {
+    title: "how to get started with django",
+    content: "This is content of django",
+    slug: "django-learn",
+  },
+  {
+    title: "how to get started with CSS",
+    content: "This is content of css",
+    slug: "css-learn",
+  },
+];
+
+module.exports = blogs;
